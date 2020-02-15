8a8dadd0bc43324fb8d3a00e550b84c07f0da431
David Torres <dftp93@gmail.com>
2020-02-15 00:36:46 -0500

    Sniptrap - LazySizes aggregate for redrawn images - js/libraries/lazysizes.min.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// document.addEventListener ("DOMContentLoaded", function(event) {
+document.addEventListener ("DOMContentLoaded", function(event) {
   
   console.log( "ready!" );
 
@@ -125,4 +125,35 @@
 
   });
 
-// });
+  $("img").each(function() {
+    
+    $(this).attr("data-src",$(this).attr("src"));
+    $(this).removeAttr("src");
+    console.log($(this)[0].outerHTML);
+  });
+  function addLazyLoad() {
+    var lazyLoad = document.querySelectorAll("img");
+    for (var i = 0; i < lazyLoad.length; i++)
+      lazyLoad[i].className += " lazyload";
+  
+  }
+  
+  addLazyLoad();
+  
+  $(document).ready(function(){
+    columnChart();
+    
+    function columnChart(){
+        var item = $('.chart', '.column-chart').find('.item'),
+        itemWidth = 100 / item.length;
+        item.css('width', itemWidth + '%');
+        
+        $('.column-chart').find('.item-progress').each(function(){
+            var itemProgress = $(this),
+            itemProgressHeight = $(this).parent().height() * ($(this).data('percent') / 100);
+            itemProgress.css('height', itemProgressHeight);
+        });
+    };
+  });
+
+});
